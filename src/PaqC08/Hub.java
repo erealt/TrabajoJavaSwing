ba2e862e4571b2f4c35a3ec954f96f322b23991d
realt <estelaretin10@gmail.com>
2023-03-12 20:34:45 +0100

    .

--- src/PaqC08/Hub.java
+++ src/PaqC08/Hub.java
@@ -67,6 +67,14 @@ public class Hub {
                     Hub[i - 1][j - 1] = Hub[i - 2][j - 1];
                 }
             }
+            c3++;
+        }else{
+            for (int i = fila; i < Hub.length; i++) {
+                for (int j = columna; j < Hub[0].length; j++) {
+                    Hub[i - 1][j - 1] = Hub[i - 2][j - 1];
+                }
+            }
+            c4++;
         }
     }
 
