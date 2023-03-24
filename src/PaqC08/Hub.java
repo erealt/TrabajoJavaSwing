a6728f092baad5234e8a18bc96bcd342997f6990
realt <estelaretin10@gmail.com>
2023-03-24 20:02:36 +0100

    Cambios en metodo apilar y desapilar

--- src/PaqC08/Hub.java
+++ src/PaqC08/Hub.java
@@ -3,10 +3,7 @@ package PaqC08;
 
 public class Hub {
     private Contenedor[][] Hub;
-    private int c1 = 9;
-    private int c2 = 9;
-    private int c3 = 9;
-    private int c4 = 3;
+
 
     public Hub() {
         Hub = new Contenedor[10][12];
@@ -23,59 +20,19 @@ public class Hub {
 
     public void apilar(Contenedor Con) {
 
-        if (Con.getPrioridad() == 1) {
-            if (c1 >= 0) {
-                Hub[c1][0] = Con;
-                c1--;
+        for (int i = 0; i <Hub[0].length ; i++) {
+            if(Con.getPrioridad()==i){ // si la prioridad del contenedor es igual a la columna del hub
+
+                    Hub[Hub.length-1][i]=Con; // se guarda el contenedor en la ultima fila de esa columna
+
             }
-        } else if (Con.getPrioridad() == 2) {
-            if (c2 >= 0) {
-                Hub[c2][1] = Con;
-                c2--;
-            }
-        } else if (Con.getPrioridad() == 3) {
-            if (c3 >= 0 && c4 <= 12) {
-                for (int i = 2; i < c4; i++) {
-                    Hub[c3][i] = Con;
-                }
-                c3--;
-                c4++;
-            }
+
         }
     }
 
     public void desapilar(int fila, int columna) {
         Hub[fila - 1][columna - 1] = null;
 
-        if (columna == 1) {
-            for (int i = fila; i < Hub.length; i++) {
-                for (int j = columna; j < Hub[0].length; j++) {
-                    Hub[i - 1][j - 1] = Hub[i - 2][j - 1];
-                }
-            }
-            c1++;
-        } else if (columna == 2) {
-            for (int i = fila; i < Hub.length; i++) {
-                for (int j = columna; j < Hub[0].length; j++) {
-                    Hub[i - 1][j - 1] = Hub[i - 2][j - 1];
-                }
-            }
-            c2++;
-        } else if (columna == 3) {
-            for (int i = fila; i < Hub.length; i++) {
-                for (int j = columna; j < Hub[0].length; j++) {
-                    Hub[i - 1][j - 1] = Hub[i - 2][j - 1];
-                }
-            }
-            c3++;
-        }else{
-            for (int i = fila; i < Hub.length; i++) {
-                for (int j = columna; j < Hub[0].length; j++) {
-                    Hub[i - 1][j - 1] = Hub[i - 2][j - 1];
-                }
-            }
-            c4++;
-        }
     }
 
     public String mostrarDatosContenedor(int NIC) {
