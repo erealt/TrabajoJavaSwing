3fc17329c54c8f71f77e9ea694782de55e567432
realt <estelaretin10@gmail.com>
2023-03-26 11:42:08 +0200

    metodo apilar_contenedor --> clase Puerto

--- src/PaqC08/Puerto.java
+++ src/PaqC08/Puerto.java
@@ -1,9 +1,12 @@
 package PaqC08;
 
-public class Puerto {
+public class Puerto extends Hub {
     private Hub[]p;
     private boolean [] duplas;
 
+    public Puerto() {
+        p = new Hub[3];
+    }
     public Hub[] getP() {
         return p;
     }
@@ -20,11 +23,25 @@ public class Puerto {
         this.duplas = duplas;
     }
 
-    public void apilar_contenedor(){
+    public void apilar_contenedor(Contenedor c){
+        for (int i = 0; i <p.length ; i++) {
+            if (p[i] == null) { // si hay un elemento con null es que esta vacio y apila ahi
+                apilar(c);
+            }
+
+            }
 
     }
     public  Hub desapilar(int hub,int columna){// tiene que devolver un Hub
         Hub h=new Hub();
+        for (int i = 0; i <p.length  ; i++) {
+            if(hub==i){
+
+                desapilar(columna);
+
+            }
+
+        }
         return h;
     }
 }
